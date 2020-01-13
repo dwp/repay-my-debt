4b23e05f30fd0889eb6863d09d4575e461fa466c
cdenno <chdenno@gmail.com>
2020-01-13 13:49:38 +0000

    scenario testing

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Manage the money you owe to DWP',
 
   // Default port that prototype runs on
   port: '3000',
