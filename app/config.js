3a8e7290bea2a171643bf0582f3023c4955e2d67
cdenno <chdenno@gmail.com>
2020-06-05 12:10:37 +0100

    mvp focus changes

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Manage the money you owe to DWP',
+  serviceName: 'Repay my debt',
 
   // Default port that prototype runs on
   port: '3000',
