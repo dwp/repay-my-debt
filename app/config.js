73246d6fa7b5e3e15478f21eaba08e6823cef5e6
cdenno <chdenno@gmail.com>
2021-10-05 12:56:00 +0100

    tweak to name

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Repay my debt to DWP',
+  serviceName: 'Repay my debt',
 
   // Default port that prototype runs on
   port: '3000',
