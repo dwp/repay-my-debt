2cb4c5c39f01a6241cc7048952c2c1ec518b12db
cdenno <chdenno@gmail.com>
2021-10-05 12:54:25 +0100

    repay my debt return

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Manage my benefit debt',
+  serviceName: 'Repay my debt to DWP',
 
   // Default port that prototype runs on
   port: '3000',
